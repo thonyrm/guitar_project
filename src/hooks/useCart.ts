50d60b02726c7393379e64549b604d88b299daef
Thony Dev <ramosm0106@gmail.com>
2025-08-31 19:08:41 -0300

    Update UserCart

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 const useCart = () =>{
+    const [] = useState();
+    const MIN_ITEM_QUANTITY = 1;
+    const MAX_ITEM_QUANTITY = 5;
+
+    function addToCart(){
+        console.log("Item added to cart");
+    }
+    function removeFromCart(){
+        console.log("Item removed from cart");
+    }
+    function decreaseQuantity(){
+        console.log("Item quantity decreased");
+    }
+    function increaseQuantity(){
+        console.log("Item quantity increased");
+    }
+    function clearCart(){
+        console.log("Cart cleared");
+    }
     
     return {
-
+        addToCart,
+        removeFromCart,
+        decreaseQuantity,
+        increaseQuantity,
+        clearCart
     };
 }
 export default useCart;
